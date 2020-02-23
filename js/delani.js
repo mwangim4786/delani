1f942a897897a1d2f57e17597f78b192c2ced26e
MARTIN <mwangim4786@gmail.com>
2020-02-23 18:55:13 +0300

    completes hover effect for first row of portfolio

--- js/delani.js
+++ js/delani.js
@@ -12,5 +12,30 @@ $(function(){
       $('.togle2').toggle();
     });
 
+
+    $('#hov1').hover(function(){
+        $('#hover1').show();
+      }, function() {
+        $('#hover1').hide()
+       });
+    
+       $('#hov2').hover(function(){
+        $('#hover2').show();
+      }, function() {
+        $('#hover2').hide()
+       });
+    
+       $('#hov3').hover(function(){
+        $('#hover3').show();
+      }, function() {
+        $('#hover3').hide()
+       });
+    
+       $('#hov4').hover(function(){
+        $('#hover4').show();
+      }, function() {
+        $('#hover4').hide()
+       });
+
 });
   
