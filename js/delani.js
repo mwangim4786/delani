14077ebc5ce58e6e41989a1cc2f1da0494ad0f58
MARTIN <mwangim4786@gmail.com>
2020-02-23 18:18:22 +0300

    adds toggle effect to what we do icons

---
+++ js/delani.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.togle').click(function(){
+      $('.togle').toggle();
+    });
+  
+    $('.togle1').click(function(){
+      $('.togle1').toggle();
+    });
+  
+    $('.togle2').click(function(){
+      $('.togle2').toggle();
+    });
+
+});
+  
